a3d92fcd6fb38dbfe6c009fca3c7db631aed4a01
Humberto <humberto@2com.com.mx>
2018-02-14 17:26:07 -0600

    Cambio de llaves para dev

--- models/Pagos.php
+++ models/Pagos.php
@@ -10,10 +10,10 @@ class Pagos
 	const OPEN_PAY = 12;
 
 	// llaves 2gom
-	// const API_OPEN_PAY = "mgvepau0yawr74pc5p5x";
-	// const API_OPEN_PAY_SECRET = "sk_b1885d10781b4a05838869f02c211d48";
-	// const API_OPEN_PAY_PUBLIC = "pk_a4208044e7e4429090c369eae2f2efb3";
-	// const API_SANDBOX = true;
+	 const API_OPEN_PAY = "mgvepau0yawr74pc5p5x";
+	 const API_OPEN_PAY_SECRET = "sk_b1885d10781b4a05838869f02c211d48";
+	 const API_OPEN_PAY_PUBLIC = "pk_a4208044e7e4429090c369eae2f2efb3";
+	 const API_SANDBOX = true;
 
 	// LLaves cliente sandbox
 	//  const API_OPEN_PAY = "mdkj2jyrw5kagur64bfk";
@@ -22,10 +22,10 @@ class Pagos
 	//  const API_SANDBOX = true;
 
 	 // Llaves cliente producción
-	 const API_OPEN_PAY = "mql4tth4ssfl4t7kvs3l";
-	 const API_OPEN_PAY_SECRET = "sk_cd52ec33376f46dc986d1fd2c4f9193c";
-	 const API_OPEN_PAY_PUBLIC = "pk_1103fff416c043f9845f3563639df8f8";
-	 const API_SANDBOX = false;
+	//  const API_OPEN_PAY = "mql4tth4ssfl4t7kvs3l";
+	//  const API_OPEN_PAY_SECRET = "sk_cd52ec33376f46dc986d1fd2c4f9193c";
+	//  const API_OPEN_PAY_PUBLIC = "pk_1103fff416c043f9845f3563639df8f8";
+	//  const API_SANDBOX = false;
 
 	/**
 	 * Generar codigo para poder pagar en las tiendas
