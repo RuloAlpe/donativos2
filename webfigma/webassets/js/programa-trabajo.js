9dfc496f5274a2cb3a6e37b9b0b207419c55b920
Damián <damian2gom@gmail.com>
2018-03-12 11:27:11 -0600

    Ajustes.. script css

--- webfigma/webassets/js/programa-trabajo.js
+++ webfigma/webassets/js/programa-trabajo.js
@@ -36,32 +36,43 @@ $(document).ready(function(){
 
 
 	// Get the modal
-var modal = document.getElementById('modal-ayuda');
+	var modal = document.getElementById('modal-ayuda');
 
-// Get the button that opens the modal
-var btn = document.getElementById("modal-open");
+	// Get the button that opens the modal
+	var btn = document.getElementById("modal-open");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("modal-close")[0];
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("modal-close")[0];
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-	modal.style.display = "-webkit-box";
-	modal.style.display = "-ms-flexbox";
-	modal.style.display = "flex";
-}
+	// When the user clicks on the button, open the modal 
+	btn.onclick = function() {
+		modal.style.display = "-webkit-box";
+		modal.style.display = "-ms-flexbox";
+		modal.style.display = "flex";
+	}
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
 
+
+
+
+});
+
+
+$(function () {
+	$('#verMas').bind("click", function () {
+		var dest = $("#one").offset().top;
+		$("html, body").animate({scrollTop: dest},600);
+	});
 
 });
