204eed0ea58e17bceb495f6df1c22ebb0aebb272
Damián <damian2gom@gmail.com>
2018-03-26 17:00:52 -0600

    Ajuste de zoom 2

--- web/webassets/js/geeks.js
+++ web/webassets/js/geeks.js
@@ -6,5 +6,21 @@ $(document).ready(function(){
         console.log("input focus ios");
     });
 
+    $("input[type=text], input[type=password], input[type=email], textarea").on({ 'touchstart' : function() {
+        zoomDisable();
+    }});
+    $("input[type=text], input[type=password], input[type=email], textarea").on({ 'touchend' : function() {
+        setTimeout(zoomEnable, 500);
+    }});
+    
+    function zoomDisable(){
+      $('head meta[name=viewport]').remove();
+      $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0" />');
+    }
+    function zoomEnable(){
+      $('head meta[name=viewport]').remove();
+      $('head').prepend('<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=1" />');
+    } 
+
 
 });
